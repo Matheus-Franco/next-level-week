e907544de2a7908039edc3c6f0336ed2eb83278c
Matheus Franco <matheusfrancoalves@hotmail.com>
2020-06-03 22:49:08 -0300

    day 2

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import cors from "cors";
+import path from "path";
+import routes from "./routes";
 
 const app = express();
 
-app.get("/users", (req, res) => {
-  console.log("User List");
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 
-  res.json(["Matheus", "Cinthya", "Naju", "Zelia"]);
-});
+app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
 
 app.listen(3333, () => {
   console.log("Backend Executando.");
